207a80394aed342a45a7703c56ea2542437920df
Felipe <felipe.m.c.b.fm@gmail.com>
2024-05-11 19:20:56 -0300

    logica dos inputs da tela de login efetuado!

--- src/TelaLogin/index.jsx
+++ src/TelaLogin/index.jsx
@@ -1,8 +1,14 @@
+import { useState } from 'react'
 import trackit from './../assets/track-it.png'
 import { Link } from 'react-router-dom'
 
 
+
 export default function TelaLogin(props){
+
+    const [valoresInput, setValoresInput] = useState({email:'', senha:''})
+    const chavesInputs = Object.keys(valoresInput)
+
     return(
         <>
             <div className="container-imagem">
@@ -10,8 +16,17 @@ export default function TelaLogin(props){
             </div>
             
             <form>
-                <input type="text" />
-                <input type="text" />
+                {chavesInputs.map((chave, index)=>(
+                    <input 
+                        key={index}
+                        type="text"
+                        value={valoresInput[chave]}
+                        onChange={(event)=>{
+                            const valor = event.target.value
+                            setValoresInput({...valoresInput, [chave] :valor})
+                        }} />
+                ))}
+                
                 <button>Entrar</button>
             </form>
 
