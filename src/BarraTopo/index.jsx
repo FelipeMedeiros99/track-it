f30dbcde3305e292d4e55c961f836812690562ce
Felipe <felipe.m.c.b.fm@gmail.com>
2024-05-11 20:11:33 -0300

    formatação básica na barra do topo

--- src/BarraTopo/index.jsx
+++ src/BarraTopo/index.jsx
@@ -1,10 +1,33 @@
+import styled from 'styled-components'
 import trackit from './track-it-text.svg'
 
 export default function BarraTopo({ link_foto }) {
     return (
-        <>
-            <img src={trackit} alt="track it" />
-            <img src={link_foto} alt="foto do usuário" />
-        </>
+        <Barra>
+            <img className='simbolo' src={trackit} alt="track it" />
+            <img className='foto-perfil' src={link_foto} alt="foto do usuário" />
+        </Barra>
     )
 }
+
+
+const Barra = styled.div`
+    display: flex;
+    align-items: center;
+    padding: 10px 18px 10px 18px;
+    justify-content: space-between;
+    height: 70px;
+    background-color: #126BA5;
+    
+    .simbolo{
+        width: 97px;
+        height: 49px;
+        color: aliceblue;
+    }
+
+    .foto-perfil{
+        height: 51px;
+        width: 51px;
+    }
+
+`
--- src/TelaHabitos/index.jsx
+++ src/TelaHabitos/index.jsx
@@ -1,5 +1,10 @@
+import BarraTopo from '../BarraTopo'
+
 export default function TelaHabitos(props){
     return(
-        <>Tela habitos</>
+        <>
+            <BarraTopo link_foto={'https://midias.correiobraziliense.com.br/_midias/jpg/2016/04/05/675x450/1_cbnfot050420160102-21057794.jpg?20220217210708?20220217210708'}/>  
+
+        </>
     )
 }
