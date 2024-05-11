3ab232e9714983fb913cc2c8a0bad74592a9f374
Felipe <felipe.m.c.b.fm@gmail.com>
2024-05-11 18:59:02 -0300

    criado o layout basico da tela de login

--- src/TelaLogin/index.jsx
+++ src/TelaLogin/index.jsx
@@ -1,5 +1,22 @@
+import trackit from './../assets/track-it.png'
+import { Link } from 'react-router-dom'
+
+
 export default function TelaLogin(props){
     return(
-        <>tela login</>
+        <>
+            <div className="container-imagem">
+                <img src={trackit} alt="Logo do Track-it" />
+            </div>
+            
+            <form>
+                <input type="text" />
+                <input type="text" />
+                <button>Entrar</button>
+            </form>
+
+            <Link to='/cadastro'> Não tem uma conta? Cadastre-se</Link>
+
+        </>
     )
 }
